710e77c0d99fb4511f8d970195e133b3e51acd2c
awilkosz <32845162+awilkosz@users.noreply.github.com>
2020-05-14 15:27:15 +0200

    Test de la classe C_Livre

---
+++ src/test/C_LIVRETest.java
@@ -0,0 +1,82 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+import objets.C_LIVRE;
+
+class C_LIVRETest {
+
+	@Before
+	public void setUp() throws Exception
+	{
+		
+	}
+	
+	@After
+	public void tearDown() throws Exception
+	{
+		
+	}
+	
+	@Test
+	//Création et suppression d'un livre
+	public void testCreationLivreBdd()
+	{
+		C_LIVRE livre = new C_LIVRE();
+		livre.setMedia_titre("Cyrano de Bergerac");
+		livre.setMedia_annee("1897");
+		livre.setMedia_type("Livre");
+		livre.setMedia_lien("unLien");
+		
+		int i = livre.creationMediaBdd(2);
+		assertEquals(1, i);
+
+		int media_id = bdd.C_requetes.rechercheId("SELECT book_id FROM book ORDER BY book_id DESC LIMIT 1");		
+		int j = livre.suppressionMediaBdd(media_id);
+		assertEquals(1, j);
+	}
+	
+	@Test
+	//Modification de l'ISBN et du synopsis du livre
+	public void testModificationLivreBdd()
+	{
+		C_LIVRE livre = new C_LIVRE();
+		livre.setMedia_titre("Cyrano de Bergerac");
+		livre.setMedia_annee("1897");
+		livre.setMedia_type("Livre");
+		livre.setMedia_lien("unLien");
+		
+		livre.setLivre_isbn("9782266152174");
+		livre.setLivre_synop("Le malheureux Cyrano de Bergerac, un poète et bretteur affublé d\\'un nez démesurément grand, aime secrètement sa ravissante cousine Roxane. Se jugeant laid, il n\\'ose lui déclarer sa flamme. La jeune femme lui confie bientôt sa passion pour un jeune cadet de Gascogne, le séduisant mais insipide Christian de Neuvillette. Quand Roxane lui fait promettre de prendre son bien-aimé sous sa protection, Cyrano accepte de se sacrifier. Il impose silence à son cœur et met son esprit et sa verve au service de son rival. Cyrano propose de lui rédiger ses mots d’amour.");
+		
+		int i = livre.creationMediaBdd(2);
+		assertEquals(1, i);
+
+		int media_id = bdd.C_requetes.rechercheId("SELECT book_id FROM book ORDER BY book_id DESC LIMIT 1");
+		int j = bdd.C_connexion.ex_Update("UPDATE book SET ISBN = " + livre.getLivre_isbn() + ", book_synop = '" + livre.getLivre_synop() + "' WHERE book_id = " + media_id);
+		assertEquals(1, j);
+				
+		int k = livre.suppressionMediaBdd(media_id);
+		assertEquals(1, k);
+	}
+	
+	@Test
+	//Création d'un livre par son id, on teste si l'ISBN du livre est le même que celui de la BDD
+	public void testCreerLivreAvecId()
+	{
+		C_LIVRE livre = new C_LIVRE().creerLivreAvecId(106);
+		
+		System.out.println(livre.toString());
+		
+		int i = 0;
+		if(livre.getLivre_isbn().equals("9782075120401"))
+			i = 1;
+		assertEquals(1, i);
+	}
+
+}
